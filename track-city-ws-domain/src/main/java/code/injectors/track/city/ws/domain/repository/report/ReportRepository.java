cab19fa0d813638ae2e115fb9a7f4a42e0a4f31f
Chatzakis Nikolaos <nik.chatzak@gmail.com>
2017-05-06 17:44:02 +0300

    Make report title field LIKE by default

--- track-city-ws-domain/src/main/java/code/injectors/track/city/ws/domain/repository/report/ReportRepository.java
+++ track-city-ws-domain/src/main/java/code/injectors/track/city/ws/domain/repository/report/ReportRepository.java
@@ -1,12 +1,22 @@
 package code.injectors.track.city.ws.domain.repository.report;
 
+import code.injectors.track.city.ws.domain.entity.report.QReport;
 import code.injectors.track.city.ws.domain.entity.report.Report;
 import code.injectors.track.city.ws.domain.repository.GenericRepository;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author Chrisostomos Bakouras
  */
 @Repository
-public interface ReportRepository extends GenericRepository<Report> {
+public interface ReportRepository extends GenericRepository<Report>, QuerydslBinderCustomizer<QReport> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QReport root) {
+        bindings.bind(root.title)
+                .first(StringExpression::containsIgnoreCase);
+    }
 }
