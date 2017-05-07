688263752d14a92d350e872880a96607f9aee8cf
Chatzakis Nikolaos <nik.chatzak@gmail.com>
2017-05-07 08:46:05 +0300

    Add Municipality like search at name

--- track-city-ws-domain/src/main/java/code/injectors/track/city/ws/domain/repository/municipality/MunicipalityRepository.java
+++ track-city-ws-domain/src/main/java/code/injectors/track/city/ws/domain/repository/municipality/MunicipalityRepository.java
@@ -1,12 +1,22 @@
 package code.injectors.track.city.ws.domain.repository.municipality;
 
 import code.injectors.track.city.ws.domain.entity.municipality.Municipality;
+import code.injectors.track.city.ws.domain.entity.municipality.QMunicipality;
 import code.injectors.track.city.ws.domain.repository.GenericRepository;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author Chrisostomos Bakouras
  */
 @Repository
-public interface MunicipalityRepository extends GenericRepository<Municipality> {
+public interface MunicipalityRepository extends GenericRepository<Municipality>, QuerydslBinderCustomizer<QMunicipality> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QMunicipality root) {
+        bindings.bind(root.name)
+                .first(StringExpression::containsIgnoreCase);
+    }
 }
